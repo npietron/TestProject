219daab65f946811fe7f1762e589fd6b595c3260
Norbert Pietroń <norbert.pietron@outlook.com>
2016-03-01 12:00:19 +0100

    web api added

---
+++ src/TestProject.Services/Concrete/CustomPostService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject.DataLayer.Repositories.Abstract;
+using TestProject.Services.Abstract;
+
+namespace TestProject.Services.Concrete
+{
+    public class CustomPostService : IPostService
+    {
+        private readonly IPostRepository _postRepository;
+
+        public CustomPostService(IPostRepository postRepository)
+        {
+            _postRepository = postRepository;
+        }
+    }
+}
---
+++ src/TestProject.Services/Concrete/CustomUserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestProject.DataLayer.Repositories.Abstract;
+using TestProject.Services.Abstract;
+
+namespace TestProject.Services.Concrete
+{
+    public class CustomUserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public CustomUserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+    }
+}
