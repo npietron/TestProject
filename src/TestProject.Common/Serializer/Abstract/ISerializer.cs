1aa6b8361daa5996153fbabc4e8f651731c41416
Norbert Pietroń <norbert.pietron@outlook.com>
2016-03-03 12:07:18 +0100

    solution cleanup

--- src/TestProject.Common/Serializer/Abstract/ISerializer.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TestProject.Common.Serializer.Abstract
-{
-    public interface ISerializer<T>
-    {
-        string Serialize(T model);
-        T Deserialize(string description);
-    }
-}
