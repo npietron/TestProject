ff47b7c141d36d2853b25137ee0e670a473d5d91
Norbert Pietroń <norbert.pietron@outlook.com>
2016-03-03 11:17:48 +0100

    Missing file

---
+++ src/TestProject.WebApi/App_Start/FormatConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace TestProject.WebApi
+{
+    public static class FormatConfig
+    {
+        public static void ManageFormats(HttpConfiguration config)
+        {
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            config.Formatters.JsonFormatter.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+
+            GlobalConfiguration.Configuration.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            GlobalConfiguration.Configuration.Formatters.Remove(GlobalConfiguration.Configuration.Formatters.XmlFormatter);
+        }
+    }
+}
